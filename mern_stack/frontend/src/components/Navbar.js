291510eb08c654bb4c75831fc552100942d94c72
Roshan Zamil <rshn.zamil@gmail.com>
2024-03-27 09:45:57 +0400

    uploaded css and img

--- mern_stack/frontend/src/components/Navbar.js
+++ mern_stack/frontend/src/components/Navbar.js
@@ -9,6 +9,28 @@ const Navbar = () => {
                 </Link>
             </div>
         </header>
+
+<div class="loader-content">
+<div class="d-table">
+    <div class="d-table-cell">
+        <div class="sk-circle">
+            <div class="sk-circle1 sk-child"></div>
+            <div class="sk-circle2 sk-child"></div>
+            <div class="sk-circle3 sk-child"></div>
+            <div class="sk-circle4 sk-child"></div>
+            <div class="sk-circle5 sk-child"></div>
+            <div class="sk-circle6 sk-child"></div>
+            <div class="sk-circle7 sk-child"></div>
+            <div class="sk-circle8 sk-child"></div>
+            <div class="sk-circle9 sk-child"></div>
+            <div class="sk-circle10 sk-child"></div>
+            <div class="sk-circle11 sk-child"></div>
+            <div class="sk-circle12 sk-child"></div>
+        </div>
+    </div>
+</div>
+</div>
+
     )
 }
 
